91a78b868cf4af43c6cfefb39e145e233d185245
JarrodCTaylor <jarrod@fastmail.com>
2023-08-07 18:04:30 -0500

    WIP

--- debug.sh
+++ debug.sh
@@ -1,3 +1,4 @@
+PROJECT=data.fressian
 
 version="$(cd repo && mvn -q -N org.codehaus.mojo:exec-maven-plugin:1.3.1:exec -Dexec.executable='echo' -Dexec.args='${project.version}' | tail -1)"
 echo "version=$version"
@@ -13,5 +14,5 @@ echo "(def PROJECT \"$PROJECT\") (def VERSION \"$version\")" > proj.clj
 cat proj.clj collect.clj | clojure -Sforce -J-Dclojure.spec.skip-macros=true -Sdeps '{:deps {org.clojure/data.fressian {:mvn/version "RELEASE"}}}' -M:collect -
 
 # Run autodoc
-### echo "Building $PROJECT"
-### cat proj.clj build.clj | clojure -M:build -
+echo "Building $PROJECT"
+cat proj.clj build.clj | clojure -M:build -
