a43cda8e87a493dd987c97f5acf85822c04e3be9
JarrodCTaylor <jarrod@fastmail.com>
2023-08-07 17:45:22 -0500

    WIP

--- debug.sh
+++ debug.sh
@@ -2,7 +2,13 @@
 version="$(cd repo && mvn -q -N org.codehaus.mojo:exec-maven-plugin:1.3.1:exec -Dexec.executable='echo' -Dexec.args='${project.version}' | tail -1)"
 echo "version=$version"
 
+echo "Where are we?"
+ls
+
 cd ..
+
+echo "Where are we now"
+ls
 
 # Copy stable site files
 echo "Copying static site files"
