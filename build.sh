0bad782cac13bca75e7dddf0f2a4ead99041cc12
JarrodCTaylor <jarrod@fastmail.com>
2023-08-08 12:39:54 -0500

    WIP

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd contrib-api-doc
 
 cp -r build.ci/* .
 
-version="$(mvn -q -N org.codehaus.mojo:exec-maven-plugin:1.3.1:exec -Dexec.executable='echo' -Dexec.args='${project.version}' | tail -1)"
+version="$(cd repo && mvn -q -N org.codehaus.mojo:exec-maven-plugin:1.3.1:exec -Dexec.executable='echo' -Dexec.args='${project.version}' | tail -1)"
 echo "version=$version"
 
 # Copy stable site files
