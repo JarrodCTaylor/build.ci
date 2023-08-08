6e78b162da3e09b9870504d71433f0935a78ba5b
JarrodCTaylor <jarrod@fastmail.com>
2023-08-08 08:03:41 -0500

    WIP

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 PROJECT="$(echo "$1" | rev | cut -d/ -f1 | rev)"
 
-version="$(cd repo && mvn -q -N org.codehaus.mojo:exec-maven-plugin:1.3.1:exec -Dexec.executable='echo' -Dexec.args='${project.version}' | tail -1)"
+cd repo
+
+version="$(mvn -q -N org.codehaus.mojo:exec-maven-plugin:1.3.1:exec -Dexec.executable='echo' -Dexec.args='${project.version}' | tail -1)"
 echo "version=$version"
 
 # Copy stable site files
