4f31dc3d45a729b22486363bcd8f03913f2cc539
JarrodCTaylor <jarrod@fastmail.com>
2023-08-08 08:15:01 -0500

    WIP

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 PROJECT="$(echo "$1" | rev | cut -d/ -f1 | rev)"
 
-tree
-
 cd repo
 
 version="$(mvn -q -N org.codehaus.mojo:exec-maven-plugin:1.3.1:exec -Dexec.executable='echo' -Dexec.args='${project.version}' | tail -1)"
@@ -9,15 +7,15 @@ echo "version=$version"
 
 # Copy stable site files
 echo "Copying static site files"
-cp -R contrib-api-doc/site/* repo-docs
+cp -R site/* repo-docs
 
 # Run autodoc-collect
 echo "Analyzing $PROJECT"
 rm -f analysis.edn
 echo "(def PROJECT \"$PROJECT\") (def VERSION \"$version\")" > proj.clj
-cat proj.clj contrib-api-doc/collect.clj | clojure -Sforce -J-Dclojure.spec.skip-macros=true -Sdeps "{:deps {org.clojure/${PROJECT} {:mvn/version \"RELEASE\"}}}" -M:collect -
+cat proj.clj collect.clj | clojure -Sforce -J-Dclojure.spec.skip-macros=true -Sdeps "{:deps {org.clojure/${PROJECT} {:mvn/version \"RELEASE\"}}}" -M:collect -
 
 # Run autodoc
 echo "Building $PROJECT"
-cat proj.clj contrib-api-doc/build.clj | clojure -M:build -
+cat proj.clj build.clj | clojure -M:build -
 
