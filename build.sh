9a2fadc876850093e3544bba728ad8e03c0193d6
JarrodCTaylor <jarrod@fastmail.com>
2023-08-08 12:24:21 -0500

    WIP

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@ PROJECT="$(echo "$1" | rev | cut -d/ -f1 | rev)"
 
 cd repo
 
+echo "What is in repo?"
+ls
+
 cp -r contrib-api-doc/* .
 
 version="$(mvn -q -N org.codehaus.mojo:exec-maven-plugin:1.3.1:exec -Dexec.executable='echo' -Dexec.args='${project.version}' | tail -1)"
